b2d7445ad497f47ca0cbc727575c7d6153bcc28c
jgilmartinez <151668462+jgilmartinez@users.noreply.github.com>
2023-11-22 09:51:35 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -7,6 +7,7 @@ makeCacheMatrix <- function(x = matrix()) {
         matrixInv <- NULL
         set <- function(y) {
                 x <<- y
+                message("load data")
                 matrixInv <<- NULL
         }
         get <- function() x
